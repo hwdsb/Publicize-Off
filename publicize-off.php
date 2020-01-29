80f88e9eb665c65b1fff24c43661d17f4fa63073
Jared Bennett <jarbenne@hwdsb.on.ca>
2020-01-29 15:53:24 -0500

    Small Edit

--- publicize-off.php
+++ publicize-off.php
@@ -15,13 +15,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-
- /* Set Publicize to be off by default */
+ // Set Publicize to be off by default
 
  if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'publicize' ) ) {
  	add_filter( 'publicize_checkbox_default', '__return_false' );
